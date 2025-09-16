64ec99fb1970e737ab3b7260fca4c71f1847c6fd
Ayush Chaudhary <ayush0000ayush@gmail.com>
2025-09-17 02:48:58 +0530

    :bug: Fix the infinite trigger loop

--- app/src/main/java/com/blurr/voice/triggers/PandaNotificationListenerService.kt
+++ app/src/main/java/com/blurr/voice/triggers/PandaNotificationListenerService.kt
@@ -24,6 +24,11 @@ class PandaNotificationListenerService : NotificationListenerService() {
         val packageName = sbn.packageName
         Log.d(TAG, "Notification posted from package: $packageName")
 
+        if (packageName == this.packageName) {
+            Log.d(TAG, "Ignoring notification from own package.")
+            return
+        }
+
         CoroutineScope(Dispatchers.IO).launch {
             val notificationTriggers = triggerManager.getTriggers()
                 .filter { it.type == TriggerType.NOTIFICATION && it.isEnabled }
